7714a7700b1bb44edbdad0609c7bd5026791744a
Vitaliy Korney <Vitaliy.Korney@enterra-inc.com>
2016-11-23 12:38:44 +0700

    expense tracker
    account implementation
    V1.

---
+++ ExpenseTracker.DataLayer/ExpenseTrackerContext.cs
@@ -0,0 +1,21 @@
+﻿using ExpenseTracker.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.DataLayer
+{
+    public class ExpenseTrackerContext:DbContext
+    {
+
+        public ExpenseTrackerContext() : base("name=ExpenseTracker")
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+
+    
+    }
+}
---
+++ ExpenseTracker.Model/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Model
+{
+    [Table("Account")]
+    public class Account
+    { 
+        [Key]
+        [Column("id_account")]
+        public int IdAccount { get; set; }
+        [Required]
+        [MaxLength(300)]
+        [Column("name")]
+        public string Name { get; set; }
+    }
+}
